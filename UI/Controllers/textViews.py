e52a3aa71f21624638404205a38d35210cbea1a7
zouqian0820 <761505506@qq.com>
2016-09-23 14:15:04 +0800

    by zouqian

--- UI/Controllers/textViews.py
+++ UI/Controllers/textViews.py
@@ -5,3 +5,7 @@ from django.shortcuts import render,HttpResponse
 
 def test(request):
     return render(request, 'layout/_layout.html')
+
+def personal(request):
+    print(123)
+    return render(request,'personal/personalpage.html')
